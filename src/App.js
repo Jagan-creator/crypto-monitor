365aba3648ec81b9d59c7e7f45c4758c0fddd312
Joel Hagan <joel.hagan.8@gmail.com>
2024-01-10 12:27:34 -0700

    set up navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { ThemeProvider } from './context/ThemeContext'
+import Navbar from "./components/Navbar";
 
 function App() {
   return <ThemeProvider>
-    
+    <Navbar />
   </ThemeProvider>
 }
 
