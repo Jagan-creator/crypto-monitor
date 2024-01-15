a8f7681b74303768de5ccb01965624c1cff75863
Joel Hagan <joel.hagan.8@gmail.com>
2024-01-15 12:47:50 -0700

    set up routes on app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { ThemeConditions } from './context/ThemeConditions'
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Account from './pages/Account'
 
 function App() {
   return <ThemeConditions>
     <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/account' element={<Account />} />
+    </Routes>
   </ThemeConditions>
 }
 
