454048812b288bb08c9923fe2ba3dde75f9456dc
Joel Hagan <joel.hagan.8@gmail.com>
2024-01-15 13:50:25 -0700

    spread API coin props throughout CoinNav page

--- src/components/CoinNav.jsx
+++ src/components/CoinNav.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
 
-function CoinNav() {
-  return <div>CoinNav</div>;
+function CoinNav({ coins }) {
+  return (
+    <div>
+      <div>
+        <h1>Search</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="find a coin"
+          />
+        </form>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>24h Volume</th>
+            <th>Mkt</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.id}>
+              <td>
+                <AiOutlineStar />
+              </td>
+              <td>{coin.market_cap_rank}</td>
+              <td>
+                <div>
+                  <img
+                    src={coin.image}
+                    alt={coin.id}
+                  />
+                  <p>{coin.name}</p>
+                </div>
+              </td>
+              <td>{coin.symbol}</td>
+              <td>{coin.current_price}</td>
+              <td>{coin.price_change_percentage_24h}</td>
+              <td>{coin.total_volume}</td>
+              <td>{coin.market_cap}</td>
+              <td>{coin.sparkline_in_7d.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default CoinNav;
