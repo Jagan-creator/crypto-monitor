c36594a68eed225a1dded7a5ebab7794f8debdc6
Joel Hagan <joel.hagan.8@gmail.com>
2024-01-16 16:33:22 -0700

    import trending coin page to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CoinNav from "../components/CoinNav";
+import TrendingCoins from "../components/TrendingCoins";
 
 function Home({ coins }) {
   return (
     <div>
       <CoinNav coins={coins} />
+      <TrendingCoins />
     </div>
   );
 }
