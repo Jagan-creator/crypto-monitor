a61f1c105b91c45ff02ce0d73dcb605bd2b29e2e
Joel Hagan <joel.hagan.8@gmail.com>
2024-01-16 16:32:32 -0700

    add data caching and import route for coinId

--- src/App.js
+++ src/App.js
@@ -7,46 +7,58 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Account from './pages/Account'
 import axios from "axios";
+import CoinInfo from "./pages/CoinInfo"
 
 function App() {
-  const [coins, setCoins] = useState([])
+  const [coins, setCoins] = useState([]);
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=en'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=en';
 
   useEffect(() => {
-    axios
-      .get(url)
-      .then((response) => {
-        setCoins(response.data);
-        // console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error.message);
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.error("Response data:", error.response.data);
-          console.error("Response status:", error.response.status);
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.error("No response received:", error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.error("Error setting up request:", error.message);
-        }
-      });
-  }, [url]);
-  
+    const cachedData = localStorage.getItem("cachedCoinsData");
 
-  return <ThemeConditions>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home coins={coins} />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/account' element={<Account />} />
-    </Routes>
-  </ThemeConditions>
+    if (cachedData) {
+      setCoins(JSON.parse(cachedData));
+    } else {
+      axios
+        .get(url)
+        .then((response) => {
+          setCoins(response.data);
+          // Cache the data in local storage
+          localStorage.setItem("cachedCoinsData", JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error.message);
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.error("Response data:", error.response.data);
+            console.error("Response status:", error.response.status);
+          } else if (error.request) {
+            // The request was made but no response was received
+            console.error("No response received:", error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.error("Error setting up request:", error.message);
+          }
+        });
+    }
+  }, [url]);
+
+  return (
+    <ThemeConditions>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home coins={coins} />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/account' element={<Account />} />
+        <Route path="/coin/:coinId" element={<CoinInfo />}>
+          <Route path=":coinId" />
+        </Route>
+      </Routes>
+    </ThemeConditions>
+  );
 }
 
 export default App;
