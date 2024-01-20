571eb41f888f8d0359f38d574774ea1cef567359
Joel Hagan <joel.hagan.8@gmail.com>
2024-01-20 11:48:57 -0700

    change on register to navigate to main page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,7 +19,7 @@ function Register() {
     setError(""); // Clearing previous error messages
     try {
       await register(email, password); // Calling the register function from AuthContext
-      navigate("/account");
+      navigate("/");
     } catch (e) {
       setError(e.message); // Handling registration errors
       console.log(e.message);
