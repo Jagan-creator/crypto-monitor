2b146cf3addbf46a89133cd76fd1dec70586d0cd
Joel Hagan <joel.hagan.8@gmail.com>
2024-01-20 11:50:05 -0700

    update navbar to show proper handles whether logged in or not

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ToggleLightDark from "./ToggleLightDark";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   // State to manage mobile menu visibility
   const [nav, setNav] = useState(false);
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
 
   const handleNav = () => {
     setNav(!nav);
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (e) {
+      console.log(e.message);
+    }
   };
 
   return (
@@ -22,21 +34,32 @@ const Navbar = () => {
         <ToggleLightDark />
       </div>
 
-      {/* Links (Visible on medium and larger screens) */}
-      <div className="hidden md:block">
-        <Link
-          to="/login"
-          className="p-4 hover:text-accent"
-        >
-          Login
-        </Link>
-        <Link
-          to="/register"
-          className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
-        >
-          Register
-        </Link>
-      </div>
+      {user?.email ? (
+        <div>
+          <Link
+            to="/account"
+            className="p-4"
+          >
+            Account
+          </Link>
+          <button onClick={handleSignOut}>Sign out</button>
+        </div>
+      ) : (
+        <div className="hidden md:block">
+          <Link
+            to="/login"
+            className="p-4 hover:text-accent"
+          >
+            Sign In
+          </Link>
+          <Link
+            to="/register"
+            className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
+          >
+            Register
+          </Link>
+        </div>
+      )}
 
       {/* Menu icon (Visible on small screens) */}
       <div
