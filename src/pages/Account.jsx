41bb9a4438a682cbee48120033d9bf50e093e319
Joel Hagan <joel.hagan.8@gmail.com>
2024-01-21 10:53:26 -0700

    adjust margins for account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CoinWatchList from "../components/CoinWatchList";
 import { UserAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Account() {
   const { user, logout } = UserAuth();
@@ -16,32 +16,36 @@ function Account() {
     }
   };
 
-  return (
-    <div className="max-w-[1140px] mx-auto">
-      <div className="flex justify-between items-center my-12 py-8 rounded-div">
-        <div>
-          <h1 className="text-2xl font-bold">Account</h1>
-          <div>
-            <p>Welcome, {user?.email}</p>
+  if (user) {
+    return (
+      <div className="max-w-[1140px] mx-auto">
+        <div className="flex justify-between items-center my-12 py-8 rounded-div">
+          <div className="ml-6">
+            <h1 className="text-2xl font-bold">Account</h1>
+            <div>
+              <p>Welcome, {user?.email}</p>
+            </div>
+          </div>
+          <div className="mr-6">
+            <button
+              onClick={handleSignOut}
+              className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl"
+            >
+              Sign Out
+            </button>
           </div>
         </div>
-        <div>
-          <button
-            onClick={handleSignOut}
-            className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl"
-          >
-            Sign Out
-          </button>
+        <div className="flex justify-between items-center my-12 py-8 rounded-div">
+          <div className="w-full min-h-[300px] m-6">
+            <h1 className="text-2xl font-bold py-4">Coin Watch List</h1>
+            <CoinWatchList />
+          </div>
         </div>
       </div>
-      <div className="flex justify-between items-center my-12 py-8 rounded-div">
-        <div className="w-full min-h-[300px]">
-          <h1 className="text-2xl font-bold py-4">Coin Watch List</h1>
-          <CoinWatchList />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <Navigate to="/register" />;
+  }
 }
 
 export default Account;
